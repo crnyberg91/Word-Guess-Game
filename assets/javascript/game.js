d73e31251f26005b52d124af12d5075616d39f84
Chris Nyberg <chrisn83@gmail.com>
2019-04-19 09:59:26 -0400

    additions to html, added dashesLine

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,48 @@
 const movieBank = ['jaws', 'alien', 'it'];
 const letterBank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const guessedLetters = [];
-const solution = [];
+const solutionArr = [];
 let winCount = 0;
 let LossCount = 0;
 console.log(movieBank);
 console.log(letterBank);
 
-//button here
+$('.start').on('click',function(){
+
+
 let guessesLeft = 10;
 const movie =
-movieBank[Math.floor(Math.random() * movieBank.length)];
-function blankLine(){
-
+    movieBank[Math.floor(Math.random() * movieBank.length)];
+function blankLine() {
 }
 
-
+const probLine = $('#problem-line');
+const dashes = '_ ';
+let solutionDashes = dashes.repeat(movie.length);
+console.log(solutionDashes);
+probLine.text(solutionDashes);
+// probLine.replaceWith()
 //display guessed letters
 document.onkeyup = function (event) {
     const userGuess = event.key;
+    const answer = (solutionArr.sort()).toEqual(movie.sort());
+    if (solution == movie) {
+        console.log('you win');
+        console.log(winCount++);
+    } else if (movie.includes(userGuess) === true) {
 
-
-    if (movie.includes(userGuess)===true) {
-        
         console.log(`${userGuess} is in there`);
-        console.log(solution)
-        console.log(guessedLetters)
-        return solution.push(userGuess) && guessedLetters.push(userGuess);
-        
-    } else if (solution === movie) {
+        console.log(solutionArr);
+        console.log(guessedLetters);
+        return solutionArr.push(userGuess) && guessedLetters.push(userGuess);
+
+    } else if (answer == movie) {
         console.log('you win');
         console.log(winCount++);
     } else if (guessesLeft === 0) {
         console.log('you lose');
     }
-    
+
     else {
         console.log(guessesLeft--);
         console.log(`${userGuess} is not`);
@@ -43,4 +51,4 @@ document.onkeyup = function (event) {
 
 }
 
-
+})
